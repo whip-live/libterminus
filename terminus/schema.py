00f43f7fa838ea9f1d62fa680ec807742e2b30e1
Alessandro De Angelis <alessandrodea22@gmail.com>
2017-07-17 15:30:52 +0200

    WIP

--- terminus/schema.py
+++ terminus/schema.py
@@ -3,6 +3,31 @@ import jwt
 from marshmallow import fields, Schema, validates, ValidationError
 
 
+class BaseSchema(Schema):
+    typology = fields.Field(required=True)
+    jwt = fields.Field(required=True)
+
+    @validates('typology')
+    def validate_typology(self, value):
+        typology = ['moto', 'bike']
+        if value not in typology:
+            raise ValidationError('Invalid Typology')
+
+    @validates('jwt')
+    def validate_jwt(self, value):
+        try:
+            jwt.decode(value, verify=False, algorithms=['HS256'])
+        except jwt.exceptions.DecodeError:
+            raise ValidationError('Invalid JWT')
+
+
+class DeviceDataSchema(BaseSchema):
+    device_id = fields.UUID(required=True)
+    recording_id = fields.UUID(required=True)
+    user_id = fields.UUID(required=True)
+    activity_id = fields.UUID(required=True)
+
+
 class PointSchema(Schema):
     sequence_id = fields.Integer()
     ele = fields.Float()
@@ -18,27 +43,8 @@ class PointSchema(Schema):
     pdop = fields.Float()
 
 
-class RecordingSchema(Schema):
+class RecordingSchema(BaseSchema):
     id = fields.UUID(required=True)
-    device_id = fields.UUID(required=True)
-    recording_id = fields.UUID(required=True)
-    user_id = fields.UUID(required=True)
-    activity_id = fields.UUID(required=True)
     started = fields.Integer(required=True)
     ended = fields.Integer(required=True)
-    typology = fields.Field(required=True)
-    jwt = fields.Field(required=True)
-    points = fields.List(fields.Nested(PointSchema()))
-
-    @validates('typology')
-    def validate_typology(self, value):
-        typology = ['moto', 'bike']
-        if value not in typology:
-            raise ValidationError('Invalid Typology')
-
-    @validates('jwt')
-    def validate_jwt(self, value):
-        try:
-            jwt.decode(value, verify=False, algorithms=['HS256'])
-        except jwt.exceptions.DecodeError:
-            raise ValidationError('Invalid JWT')
+    points = fields.Nested(PointSchema, many=True)
--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,22 +1,46 @@
 import datetime
 
-from terminus.schema import RecordingSchema
+from terminus.schema import BaseSchema, DeviceDataSchema, RecordingSchema
 
 
-def test_schema_with_valid_data():
+def test_base_schema(encoded_jwt):
+    """
+    Test the base schema
+    """
+    data = {
+        'jwt': encoded_jwt,
+        'typology': 'moto'
+    }
+    result = BaseSchema().load(data)
+    assert result.errors == {}
+
+
+def test_device_data_schema(encoded_jwt):
+    """
+    Test the device_data schema
+    """
+    data = {
+        'jwt': encoded_jwt,
+        'typology': 'bike',
+        'device_id': '8dc51b75-03d1-4140-9768-10a3586882e9',
+        'recording_id': 'fd3fe49e-2c5a-4c24-a317-18c057c8c036',
+        'user_id': '6fb46cfd-bb6e-4ff3-9b16-bbfbd2a45164',
+        'activity_id': '82051d4a-1616-4016-9560-cd2a1755a8ea',
+    }
+    result = DeviceDataSchema().load(data)
+    assert result.errors == {}
+
+
+def test_schema_with_valid_data(encoded_jwt):
     """
     Test recording schema with invalid JWT
     """
     data = {
         'id': '01a61386-53ae-43bd-8586-d09acf88b391',
-        'device_id': '5decc93d-ea32-41c4-9bf8-82e1d74c772f',
-        'activity_id': '0b119ed7-7333-46a0-ada1-c469a610ddd0',
-        'user_id': 'efbbb33c-f143-4f2d-a2f4-9d5d41df7d85',
-        'recording_id': 'd856a420-a04f-4328-b22e-d8f7bb12904a',
         'started': 232323,
         'ended': 23232,
         'typology': 'moto',
-        'jwt': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmb28iOiJiYXIiLCJuYW1lIjoiYWxkbyJ9.ZkBDRTfnQhJ6jxU2JtO7a0mAvL7S7smufpivvMO4mt8', # noqa
+        'jwt': encoded_jwt,
         'points': [
             {
                 'sequence_id': 1,
@@ -55,10 +79,6 @@ def test_schema_with_invalid_jwt():
     """
     data = {
         'id': '01a61386-53ae-43bd-8586-d09acf88b391',
-        'device_id': '5decc93d-ea32-41c4-9bf8-82e1d74c772f',
-        'activity_id': '0b119ed7-7333-46a0-ada1-c469a610ddd0',
-        'user_id': 'efbbb33c-f143-4f2d-a2f4-9d5d41df7d85',
-        'recording_id': 'd856a420-a04f-4328-b22e-d8f7bb12904a',
         'started': 232323,
         'ended': 23232,
         'typology': 'moto',
@@ -95,7 +115,7 @@ def test_schema_with_invalid_jwt():
     assert result.errors == {'jwt': ['Invalid JWT']}
 
 
-def test_schema_with_invalid_typology():
+def test_schema_with_invalid_typology(encoded_jwt):
     """
     Test recording schema with invalid typology
     """
@@ -108,7 +128,7 @@ def test_schema_with_invalid_typology():
         'started': 232323,
         'ended': 23232,
         'typology': 'foo',
-        'jwt': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmb28iOiJiYXIiLCJuYW1lIjoiYWxkbyJ9.ZkBDRTfnQhJ6jxU2JtO7a0mAvL7S7smufpivvMO4mt8', # noqa
+        'jwt': encoded_jwt,
         'points': [
             {
                 'sequence_id': 1,
