952a319034a88f4fab8cadcfe9bfc8d5a446c382
Federico Frenguelli <synasius@gmail.com>
2020-06-29 17:19:00 +0200

    Fixed WKP1-27: RecordingAdapter now expect an instance of recording_pb2.Recording as recording extra parameter

--- terminus/logutils.py
+++ terminus/logutils.py
@@ -1,5 +1,6 @@
 import jwt
 import logging
+import uuid
 
 from logging.handlers import DatagramHandler
 
@@ -10,11 +11,11 @@ def get_user_from_jwt(token):
     """
     Decode the JWT to read the payload
     """
-    decoded_payload = jwt.decode(token, verify=False, algorithms=['HS256'])
-    if 'username' in decoded_payload:
-        return decoded_payload['username']
+    decoded_payload = jwt.decode(token, verify=False, algorithms=["HS256"])
+    if "username" in decoded_payload:
+        return decoded_payload["username"]
     else:
-        return decoded_payload['user_id']
+        return decoded_payload["user_id"]
 
 
 class RecordingAdapter(logging.LoggerAdapter):
@@ -23,23 +24,28 @@ class RecordingAdapter(logging.LoggerAdapter):
     """
 
     def process(self, msg, kwargs):
-        extra = kwargs.get('extra', {})
-        if 'recording' in kwargs:
-            extra['device_id'] = kwargs['recording']['device_id']
-            extra['recording_id'] = kwargs['recording']['id']
-            # add to logging the JWT payload
-            extra['jwt'] = get_user_from_jwt(kwargs['recording']['jwt'])
+        extra = kwargs.get("extra", {})
+        if "recording" in kwargs:
+            # We expect recording to be an instance of recording_pb2.Recording
+            recording = kwargs["recording"]
+            # The UUIDs are serialized as array of bytes instead of strings
+            # because they occupy half the size.
+            # See `messages/terminus/proto/recording.proto`
+            extra["device_id"] = str(uuid.UUID(bytes=recording.device_id))
+            extra["recording_id"] = str(uuid.UUID(bytes=recording.id))
+            # Add to extra metadata the JWT payload
+            extra["user"] = get_user_from_jwt(recording.jwt)
 
-        elif 'topic' in kwargs:
-            extra['topic'] = kwargs['topic']
-        elif 'device_id' in kwargs:
-            extra['device_id'] = kwargs['device_id']
-        elif 'recording_id' in kwargs:
-            extra['recording_id'] = kwargs['recording_id']
-        elif 'jwt' in kwargs:
-            extra['jwt'] = get_user_from_jwt(kwargs['jwt'])
+        elif "topic" in kwargs:
+            extra["topic"] = kwargs["topic"]
+        elif "device_id" in kwargs:
+            extra["device_id"] = kwargs["device_id"]
+        elif "recording_id" in kwargs:
+            extra["recording_id"] = kwargs["recording_id"]
+        elif "jwt" in kwargs:
+            extra["user"] = get_user_from_jwt(kwargs["jwt"])
 
-        return msg, {'extra': extra}
+        return msg, {"extra": extra}
 
 
 class JSONFormatter(jsonlogger.JsonFormatter):
@@ -48,17 +54,18 @@ class JSONFormatter(jsonlogger.JsonFormatter):
     We use datadog for logging, and the datadog agent can't decode the
     default python pickled LogRecord. It can't decode a packed json either.
     """
+
     service = None
 
     def __init__(self, *args, **kwargs):
-        self.service = kwargs.pop('service', 'unknown service')
+        self.service = kwargs.pop("service", "unknown service")
 
         super().__init__(*args, **kwargs)
 
     def add_fields(self, log_record, record, message_dict):
         super().add_fields(log_record, record, message_dict)
 
-        log_record['service'] = self.service
+        log_record["service"] = self.service
 
 
 class JSONDatagramHandler(DatagramHandler):
@@ -70,26 +77,26 @@ class JSONDatagramHandler(DatagramHandler):
         super().__init__(host, port)
 
         supported_keys = [
-            'asctime',
-            'created',
-            'filename',
-            'funcName',
-            'levelname',
-            'levelno',
-            'lineno',
-            'module',
-            'msecs',
-            'message',
-            'name',
-            'pathname',
-            'process',
-            'processName',
-            'relativeCreated',
-            'thread',
-            'threadName'
+            "asctime",
+            "created",
+            "filename",
+            "funcName",
+            "levelname",
+            "levelno",
+            "lineno",
+            "module",
+            "msecs",
+            "message",
+            "name",
+            "pathname",
+            "process",
+            "processName",
+            "relativeCreated",
+            "thread",
+            "threadName",
         ]
-        custom_format = ' '.join('%({0:s})'.format(k) for k in supported_keys)
+        custom_format = " ".join("%({0:s})".format(k) for k in supported_keys)
         self.formatter = JSONFormatter(custom_format, service=service)
 
     def makePickle(self, record):
-        return self.formatter.format(record).encode() + b'\n'
+        return self.formatter.format(record).encode() + b"\n"
--- tests/test_logutils.py
+++ tests/test_logutils.py
@@ -1,3 +1,4 @@
+import uuid
 import logging
 import jwt as pyjwt
 import json
@@ -5,23 +6,32 @@ import json
 from unittest.mock import patch
 
 from terminus import RecordingAdapter, setup_logging
+from terminus.proto import recording_pb2
 
 
 def test_logging_adapter(encoded_jwt):
     """
     Test logging with `recording` argument
     """
-    recording = {
-        'recording': {
-            'device_id': 123,
-            'id': 46,
-            'jwt': encoded_jwt
-        }
-    }
-    logger = logging.getLogger()
-    adapter = RecordingAdapter(logger, None)
-    log_result = adapter.process('test msg', recording)
-    expected = ('test msg', {'extra': {'device_id': 123, 'recording_id': 46, 'jwt': 'user'}})
+    recording_id = uuid.uuid4()
+    device_id = uuid.uuid4()
+    recording = recording_pb2.Recording()
+    recording.id = recording_id.bytes
+    recording.device_id = device_id.bytes
+    recording.jwt = encoded_jwt
+    adapter = RecordingAdapter(logging.getLogger(), None)
+    log_result = adapter.process("test msg", {"recording": recording})
+
+    expected = (
+        "test msg",
+        {
+            "extra": {
+                "device_id": str(device_id),
+                "recording_id": str(recording_id),
+                "user": "user",
+            }
+        },
+    )
     assert expected == log_result
 
 
@@ -29,13 +39,10 @@ def test_logging_adapter_topic():
     """
     Test logging with `topic` argument
     """
-    topic = {
-        'topic': 'test topic'
-    }
-    logger = logging.getLogger()
-    adapter = RecordingAdapter(logger, None)
-    log_result = adapter.process('test msg', topic)
-    exepcted = ('test msg', {'extra': {'topic': 'test topic'}})
+    topic = {"topic": "test topic"}
+    adapter = RecordingAdapter(logging.getLogger(), None)
+    log_result = adapter.process("test msg", topic)
+    exepcted = ("test msg", {"extra": {"topic": "test topic"}})
     assert log_result == exepcted
 
 
@@ -43,13 +50,11 @@ def test_logging_adapter_jwt(encoded_jwt):
     """
     Test logging with `jwt` argument
     """
-    jwt = {
-        'jwt': encoded_jwt
-    }
+    jwt = {"jwt": encoded_jwt}
     logger = logging.getLogger()
     adapter = RecordingAdapter(logger, None)
-    log_result = adapter.process('test msg', jwt)
-    expected = ('test msg', {'extra': {'jwt': 'user'}})
+    log_result = adapter.process("test msg", jwt)
+    expected = ("test msg", {"extra": {"user": "user"}})
     assert log_result == expected
 
 
@@ -57,13 +62,11 @@ def test_logging_adapter_recording_id():
     """
     Test logging with `recording_id` argument
     """
-    recording_id = {
-        'recording_id': 134
-    }
+    recording_id = {"recording_id": 134}
     logger = logging.getLogger()
     adapter = RecordingAdapter(logger, None)
-    log_result = adapter.process('test msg', recording_id)
-    expected = ('test msg', {'extra': {'recording_id': 134}})
+    log_result = adapter.process("test msg", recording_id)
+    expected = ("test msg", {"extra": {"recording_id": 134}})
     assert log_result == expected
 
 
@@ -71,48 +74,51 @@ def test_logging_adapter_device_id():
     """
     Test logging with `device_id` argument
     """
-    device_id = {
-        'device_id': 1
-    }
+    device_id = {"device_id": 1}
     logger = logging.getLogger()
     adapter = RecordingAdapter(logger, None)
-    log_result = adapter.process('test msg', device_id)
-    expected = ('test msg', {'extra': {'device_id': 1}})
+    log_result = adapter.process("test msg", device_id)
+    expected = ("test msg", {"extra": {"device_id": 1}})
     assert log_result == expected
 
 
 def test_jwt_without_username():
-    data = {
-        'email': 'test@test.com',
-        'user_id': '123456'
-    }
-    encoded_jwt = pyjwt.encode(data, '123456', algorithm='HS256')
-    jwt = {
-        'jwt': encoded_jwt
-    }
+    data = {"email": "test@test.com", "user_id": "123456"}
+    encoded_jwt = pyjwt.encode(data, "123456", algorithm="HS256")
+    jwt = {"jwt": encoded_jwt}
     logger = logging.getLogger()
     adapter = RecordingAdapter(logger, None)
-    log_result = adapter.process('test msg', jwt)
-    expected = ('test msg', {'extra': {'jwt': '123456'}})
+    log_result = adapter.process("test msg", jwt)
+    expected = ("test msg", {"extra": {"user": "123456"}})
     assert log_result == expected
 
 
-@patch('terminus.logutils.JSONDatagramHandler.send')
+@patch("terminus.logutils.JSONDatagramHandler.send")
 def test_loghadler_send_JSON(fake_send, encoded_jwt):
     logger = logging.getLogger(__name__)
     logger = RecordingAdapter(logger, None)
 
-    setup_logging('test_terminus', service_level='DEBUG', ddagent_host='localhost', ddagent_port='10518')
-    recording = {'id': '123456', 'jwt': encoded_jwt, 'device_id': '098'}
+    setup_logging(
+        "test_terminus",
+        service_level="DEBUG",
+        ddagent_host="localhost",
+        ddagent_port="10518",
+    )
+    recording_id = uuid.uuid4()
+    device_id = uuid.uuid4()
+    recording = recording_pb2.Recording()
+    recording.id = recording_id.bytes
+    recording.device_id = device_id.bytes
+    recording.jwt = encoded_jwt
 
-    logger.error('Test log message', recording=recording)
+    logger.error("Test log message", recording=recording)
 
     fake_send.assert_called()
     log_record = json.loads(fake_send.call_args_list[0][0][0].decode())
-    assert log_record['levelname'] == 'ERROR'
-    assert log_record['message'] == 'Test log message'
-    assert log_record['name'] == 'tests.test_logutils'
-    assert log_record['service'] == 'test_terminus'
-    assert log_record['recording_id'] == recording['id']
-    assert log_record['jwt'] == 'user'
-    assert log_record['device_id'] == '098'
+    assert log_record["levelname"] == "ERROR"
+    assert log_record["message"] == "Test log message"
+    assert log_record["name"] == "tests.test_logutils"
+    assert log_record["service"] == "test_terminus"
+    assert log_record["recording_id"] == str(recording_id)
+    assert log_record["user"] == "user"
+    assert log_record["device_id"] == str(device_id)
